07f5910ab9096bc3ffd729af103875df81522137
juan <juan@1DAW-12>
2023-11-28 12:27:22 +0100

    Mi primer commit

---
+++ src/ud2ED/EJEMPLO2.java
@@ -0,0 +1,18 @@
+package ud2ED;
+
+public class EJEMPLO2 {
+
+	public static void main(String[] args) {
+		
+		int n = 5;
+		int sum = 0;
+		for (int i = 1; i <= n; i++) {
+		sum += i;
+		System.out.println("Valor de 'i': " + i);
+		System.out.println("Valor de 'sum': " + sum);
+		}
+		System.out.println("La suma de los números del 1 al " + n + " es: " + sum);
+	
+	
+	}
+}
---
+++ src/ud2ED/Ejemplo4.java
@@ -0,0 +1,18 @@
+package ud2ED;
+
+import java.util.Scanner;
+
+public class Ejemplo4 {
+	
+
+	public static void main(String[] args) {
+		Scanner entrada=new Scanner(System.in);
+		
+		
+		System.out.print("Introduce un número: ");
+		int number = Scanner.nextInt();
+		System.out.println("Has introducido: " + number);
+
+	}
+
+}
